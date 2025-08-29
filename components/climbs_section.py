238753dc6a22f558d3b8dd6b7fda9687ca7a340e
MichalMakowiejczuk <126947935+MichalMakowiejczuk@users.noreply.github.com>
2025-08-29 15:58:01 +0200

    Improve UI for climbs and stats sections
    Wrap elevation profiles in an expander in climbs_section.py for better organization. In stats_section.py, add a new render_main_stats_and_map function with metrics in an expander and update the original function name for clarity.

--- components/climbs_section.py
+++ components/climbs_section.py
@@ -39,53 +39,53 @@ def render_climbs_section(
         )
         st.dataframe(climbs_table, use_container_width=True)
 
-    st.subheader("Elevation profiles of detected climbs")
-    tab_titles = [f"Climb {i+1}" for i in range(len(climbs_df))]
-    tabs = st.tabs(tab_titles)
+    with st.expander("Elevation profiles of detected climbs"):
+        tab_titles = [f"Climb {i+1}" for i in range(len(climbs_df))]
+        tabs = st.tabs(tab_titles)
 
-    for i, row in enumerate(climbs_df.itertuples()):
-        with tabs[i]:
-            climb_df = track_df.iloc[
-                track_df["km"]
-                .sub(row.start_km)
-                .abs()
-                .idxmin() : track_df["km"]
-                .sub(row.end_km)
-                .abs()
-                .idxmin()
-                + 1
-            ].reset_index(drop=True)
+        for i, row in enumerate(climbs_df.itertuples()):
+            with tabs[i]:
+                climb_df = track_df.iloc[
+                    track_df["km"]
+                    .sub(row.start_km)
+                    .abs()
+                    .idxmin() : track_df["km"]
+                    .sub(row.end_km)
+                    .abs()
+                    .idxmin()
+                    + 1
+                ].reset_index(drop=True)
 
-            if len(climb_df) < 3:
-                st.info("Too short climb segment to display profile.")
-                continue
+                if len(climb_df) < 3:
+                    st.info("Too short climb segment to display profile.")
+                    continue
 
-            climb_df["km"] -= climb_df["km"].iloc[0]
-            seg_unit_km = 0.25 if row.length_m < 3000 else 0.5
+                climb_df["km"] -= climb_df["km"].iloc[0]
+                seg_unit_km = 0.25 if row.length_m < 3000 else 0.5
 
-            climb_profile = ElevationProfile(
-                climb_df, seg_unit_km=seg_unit_km, smooth_window=smooth_window
-            )
-            fig_c, ax_c = climb_profile.plot_profile(
-                show_labels=False,
-                show_background=False,
-                slope_thresholds=slope_thresholds,
-                slope_type="segment",
-            )
-            ax_c.set_ylim(climb_df["elevation"].min(), climb_df["elevation"].max())
-
-            col1, col2 = st.columns([1, 3])
-            with col1:
-                st.metric("Length", f"{row.length_m:.0f} m")
-                st.caption(
-                    f"**from** {row.start_km:.2f} km **to** {row.end_km:.2f} km of route."
+                climb_profile = ElevationProfile(
+                    climb_df, seg_unit_km=seg_unit_km, smooth_window=smooth_window
                 )
-                st.metric("Elevation gain", f"{row.gain_m:.0f} m")
-                st.metric("AVG. slope", f"{row.avg_grade_pct} %")
-                st.metric(
-                    "Climb category",
-                    classify_climb_difficulty(row.length_m, row.avg_grade_pct)[0],
+                fig_c, ax_c = climb_profile.plot_profile(
+                    show_labels=False,
+                    show_background=False,
+                    slope_thresholds=slope_thresholds,
+                    slope_type="segment",
                 )
-            with col2:
-                st.pyplot(fig_c, use_container_width=True)
-            plt.close(fig_c)
+                ax_c.set_ylim(climb_df["elevation"].min(), climb_df["elevation"].max())
+
+                col1, col2 = st.columns([1, 3])
+                with col1:
+                    st.metric("Length", f"{row.length_m:.0f} m")
+                    st.caption(
+                        f"**from** {row.start_km:.2f} km **to** {row.end_km:.2f} km of route."
+                    )
+                    st.metric("Elevation gain", f"{row.gain_m:.0f} m")
+                    st.metric("AVG. slope", f"{row.avg_grade_pct} %")
+                    st.metric(
+                        "Climb category",
+                        classify_climb_difficulty(row.length_m, row.avg_grade_pct)[0],
+                    )
+                with col2:
+                    st.pyplot(fig_c, use_container_width=True)
+                plt.close(fig_c)
--- components/stats_section.py
+++ components/stats_section.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-def render_main_stats_and_map(stats, base_map) -> None:
+def render_main_stats_and_map2(stats, base_map) -> None:
     col1, col2 = st.columns([1, 5])
     with col1:
         st.subheader("Map and stats")
@@ -14,3 +14,27 @@ def render_main_stats_and_map(stats, base_map) -> None:
     with col2:
         map_html = base_map.get_root().render()
         components.html(map_html, height=550, width=2000)
+
+
+def render_main_stats_and_map(stats, base_map) -> None:
+    with st.expander("Show map and main stats", expanded=True):
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col1:
+            st.metric("Distance", f"{stats['distance_km']:.2f} km", border=True)
+        with col2:
+            st.metric("Total elevation gain", f"{stats['ascent_m']:.0f} m", border=True)
+        with col3:
+            st.metric(
+                "Total elevation loss", f"{stats['descent_m']:.0f} m", border=True
+            )
+        with col4:
+            st.metric(
+                "Highest point", f"{stats['highest_point']:.0f} m n.p.m.", border=True
+            )
+        with col5:
+            st.metric(
+                "Lowest point", f"{stats['lowest_point']:.0f} m n.p.m.", border=True
+            )
+
+        map_html = base_map.get_root().render()
+        components.html(map_html, height=550)
